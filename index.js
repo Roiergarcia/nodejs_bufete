fe8018beb4967c7e7ca9aa6d558075830fbcb574
Roiergarcia <reports.hbo@gmail.com>
2018-10-10 23:14:07 -0500

    septimocommit
    octavo

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
-var port = process.env.PORT || 3525;
+var PORT = 3000;
 
-// Convierte una petición recibida (POST-GET...) a objeto JSON
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.get('/', function(req, res){
-	res.status(200).send({
-		message: 'GET Home route working fine!'
-	});
+app.get('/',function(req,res){
+ res.send('hola')
 });
 
-app.listen(port, function(){
-	console.log('Server running in http://localhost:${port}');
-	console.log('Defined routes:');
-	console.log('	[GET] http://localhost:3525/');
+app.listen(PORT, function() {
+ console.log('server running on http://localhost:' + PORT);
 });
